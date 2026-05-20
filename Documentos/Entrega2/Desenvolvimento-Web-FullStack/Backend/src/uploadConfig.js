72ba7e4d8319d69c521457b352118fa159093b7e
FabrizzioPuttini <fabrizzioputtini@gmail.com>
2026-05-20 15:00:06 -0300

    Arrrumando logs

--- Documentos/Entrega2/Desenvolvimento-Web-FullStack/Backend/src/uploadConfig.js
+++ Documentos/Entrega2/Desenvolvimento-Web-FullStack/Backend/src/uploadConfig.js
@@ -3,10 +3,11 @@ import path from 'path';
 import fs from 'fs';
 
 const uploadDir = path.resolve('uploads');
-
+/*
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
+*/
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
